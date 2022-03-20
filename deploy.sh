ac35301f1a5100ba451244eba442e6ab6f10969d
Mohamed Hany <mohamedhyouns@gmail.com>
2022-03-20 20:02:39 +0200

    first commit

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+cd ..
+git clone https://github.com/mohamedhany01/mohamedhany01.github.io
+cd personal-website-src || exit 0
+
+npm run build
+
+cp -r ./dist/* ../mohamedhany01.github.io
+cd ../mohamedhany01.github.io || exit 0
+
+git add .
+echo "Commite Message?"
+read commitMsg
+git commit -m "${commitMsg}"
+git push
+
+cd ../personal-website-src || exit 0
+rm -rf dist
